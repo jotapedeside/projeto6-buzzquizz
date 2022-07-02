ddf7e74672b32031ad77ec9d6a489cf8953e7fdb
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-07-02 17:13:02 -0300

    template perguntas e niveis

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
-
-
-
+const conteiner = document.querySelector('.conteiner');
+let quantidadeNiveis;
+let quantidadePerguntas;
+let quizz = {
+	title: "",
+	image: "",
+	questions: [],
+	levels: []
+}
 
 
 
 
 
 function criarQuizz () {
-    const conteiner = document.querySelector('.conteiner');
     conteiner.innerHTML = `
         <div class="criarInfoBasicas">
             <span>Comece pelo começo</span>
@@ -61,14 +66,147 @@ function validarInfoBasica (){
     }
 
     if(tituloValido && urlValida && quantidadePerguntasValida && quantidadeNiveisValido){
-        console.log('td certo');
+        quizz.title = input[0].value;
+        quizz.image = input[1].value;
+        quantidadePerguntas = input[2].value;
+        quantidadeNiveis = input[3].value;
+        criarPerguntas();
     }else{
         alert('Preencha os dados novamente.')
     }
 
+
+}
+
+function criarPerguntas(){
+    conteiner.innerHTML = `
+        <div class="criarInfoPerguntas">
+            <span>Crie suas perguntas</span>
+
+            <ul>
+
+            </ul>
+
+            <button onclick="validarPerguntas ()">Prosseguir pra criar níveis</button>
+        </div>
+    ` 
+
+    const ul = document.querySelector('ul');
+    for (let i = 0; i < quantidadePerguntas; i++){
+        ul.innerHTML += `
+                <li class="conteinerPergunta"> 
+                            
+                    <div class="headerPergunta">
+                        <span>Pergunta ${i + 1}</span>
+                        <ion-icon name="open-outline" onclick="escondeInfo(this.parentNode.parentNode,this)"></ion-icon>
+                    </div>
+                    <div class="inputPergunta escondido">
+
+                        <div class="textoPergunta">
+                            <input type="text" placeholder="Texto da pergunta">
+                            <input type="text" placeholder="Cor de fundo da pergunta">
+                        </div>
+
+                        <span>Resposta correta</span>
+                        <div class="respCorreta">
+                            <input type="text" placeholder="Resposta correta">
+                            <input type="text" placeholder="URL da imagem">
+                        </div>
+
+                        <span>Respostas incorretas</span>
+                        <div class="respIncorreta">
+                            <div>
+                                <input type="text" placeholder="Resposta incorreta 1">
+                                <input type="text" placeholder="URL da imagem 1">
+                            </div>
+                            <div>
+                                <input type="text" placeholder="Resposta incorreta 2">
+                                <input type="text" placeholder="URL da imagem 2">
+                            </div>
+                            <div>
+                                <input type="text" placeholder="Resposta incorreta 3">
+                                <input type="text" placeholder="URL da imagem 3">
+                            </div>
+
+                        </div>
+                    </div>
+                                
+                </li>
+        `
+    }
 }
 
 
-function infoNiveis (quantidadeNiveis){
+function validarPerguntas(){
+    let arrayPerguntas = document.querySelectorAll('li');
+    let perguntasValidas = [];
+    for(let i = 0; i < quantidadePerguntas; i++){
+        let inputs = arrayPerguntas[i].querySelectorAll('input');
+        let textoValido;
+        let corValida;
+        let corretaValida;
+        let imgCorretaValida;
+        let incorretasValida;
+
+        if(inputs[0].length > 19){
+            textoValido = true
+        }
+    }
+
+}
+
+function criarNiveis (){
+    conteiner.innerHTML = `
+    <div class="criarInfoNiveis">
+        <span>Agora, decida os níveis</span>
+
+        <ul>
+        </ul>
+
+        <button onclick="validarNiveis ()">Finalizar Quizz</button>
+    </div>
+    `
+
+    const ul = document.querySelector('ul');
+    for (let i = 0; i < quantidadeNiveis; i++){
+        ul.innerHTML += `
+            <li class="conteinerNivel"> 
+                        
+                <div class="headerNivel">
+                    <span>Nivel ${i + 1}</span>
+                    <ion-icon name="open-outline" onclick="escondeInfo(this.parentNode.parentNode,this)"></ion-icon>
+                </div>
+                <div class="inputNivel escondido">
+                    <input type="text" placeholder="Título do nível">
+                    <input type="text" placeholder="% de acerto mínima">
+                    <input type="text" placeholder="URL da imagem do nível">
+                    <input type="text" placeholder="Descrição do nível">
+                </div>
+                            
+            </li>
+        `
+    }
+}
+
+function escondeInfo(conteiner,icon){
+    let inputs = conteiner.querySelectorAll('div')[1];
+    let temSelecionado = document.querySelector('li.expandido');
+
+    if(temSelecionado !== null){
+        let inputExpandido = temSelecionado.querySelectorAll('div')[1];
+        inputExpandido.classList.toggle('escondido');
+        temSelecionado.querySelector('ion-icon').classList.remove('escondido');
+        temSelecionado.classList.remove('expandido')
+
+    }
+
+
+    icon.classList.add('escondido');
+    conteiner.classList.add('expandido')
+
     
+    inputs.classList.toggle('escondido');
+
+
 }
+
